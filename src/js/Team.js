946745fc41e8165de00f229278b39d181943b467
a-naraikin <anna.naraikin@yandex.ru>
2021-09-30 15:29:16 +0300

    add class Team and tests

---
+++ src/js/Team.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-else-return */
+/* eslint-disable no-plusplus */
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error('Персонаж уже добавлен в команду');
+    }
+
+    this.members.add(character);
+  }
+
+  addAll(...characters) {
+    characters.forEach((character) => this.members.add(character));
+  }
+
+  toArray() {
+    const array = [];
+    this.members.forEach((elem) => { array.push(elem); });
+    return array;
+  }
+}
